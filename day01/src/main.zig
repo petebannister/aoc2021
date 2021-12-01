2b3650068a48083415e7be7cd1c0e3bc8df387d1
Pete Bannister <pw.bannister@googlemail.com>
2021-12-01 20:00:42 +0000

    Got debugger + intellisense to work on Windows.

--- day01/src/main.zig
+++ day01/src/main.zig
@@ -4,18 +4,18 @@ const fs = std.fs;
 pub fn main() anyerror!void {
     const stdout = std.io.getStdOut().writer();
     const fname = "input.txt";
-    var file = try fs.cwd().openFile(fname, fs.File.OpenFlags{ .read = true });
-    defer file.close();
     var buf: [1024]u8 = undefined;
-    const reader = std.io.bufferedReader(file.reader()).reader();
     var part1 : i32 = 0;
     var part2 : i32 = 0;
     var index : u32 = 0;
     var last: i32 = 0;
     var last_sum: i32 = 0;
     var window = [3]i32 {0,0,0};
+
+    var file = try fs.cwd().openFile(fname, fs.File.OpenFlags{ .read = true });
+    defer file.close();
+    const reader = std.io.bufferedReader(file.reader()).reader();
     while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        // Parse the line, copy any needed bytes due to shared buffer
         var value = try std.fmt.parseInt(i32, line, 10);
         if (value > last) {
             if (index > 0) {
