68ebdedb937bbd44de39281f03c6877909cd342e
petebannister <pete.bannister@gems.co.uk>
2021-12-14 10:43:59 +0000

    part2

--- day14/main.cpp
+++ day14/main.cpp
@@ -1,37 +1,71 @@
 #include "utils.h"
 
-void solveFile(char const* fname) {
+using namespace std;
+
+using Bond = std::pair<char, char>;
+using Counts = std::map<char, uint64_t>;
+using Key = pair<Bond, uint16_t>;
+
+struct Reaction
+{
+	std::vector<char> polymer;
+	std::map<Bond, char> rules;
+	map<Key, Counts> memo;
+
+	void merge(Counts& c, Counts const& rhs) {
+		for (auto [key, value] : rhs) {
+			c[key] += value;
+		}
+	}
+
+	Counts const& polymerize(Bond bond, uint32_t n) {
+		auto key = Key(bond, n);
+		auto& c = memo[key];
+		if (c.empty()) {
+			if (auto r = rules[bond]) {
+				if (n == 0) {
+					c[r] = 1;
+				}
+				else {
+					c = polymerize(Bond(bond.first, r), n - 1);
+					merge(c, polymerize(Bond(r, bond.second), n - 1));
+					++c[r];
+				}
+			}
+		}
+		return c;
+	}
+
+	Counts iterate(uint32_t n) {
+		Counts counts;
+		for (auto e : polymer) {
+			++counts[e];
+		}
+		for (auto i : integers(polymer.size() - 1)) {
+			auto const& c = polymerize(Bond(polymer[i], polymer[i + 1]), n - 1);
+			merge(counts, c);
+		}
+		return counts;
+	}
+};
+
+
+void solveFile(char const* fname, uint32_t iterations) {
 	// Parse
 	TextFileIn f(fname);
-	std::list<char> polymer;
-	std::map<std::pair<char, char>, char> rules;
+
 	StringView sv;
 	f.readLine(sv);
-	polymer.assign(sv.begin(), sv.end());
+	Reaction reaction;
+	reaction.polymer.assign(sv.begin(), sv.end());
 	f.readLine(sv);
 	for (auto line : f.lines()) {
 		auto a = line[0];
 		auto b = line[1];
 		auto v = line[6];
-		rules[std::pair(a, b)] = v;
+		reaction.rules[std::pair(a, b)] = v;
 	}
-	for (auto i : integers(10)) {
-		auto i = polymer.begin();
-		auto e = polymer.end();
-		char last = *i;
-		++i;
-		while (i != e) {
-			if (auto r = rules[std::pair(last, *i)]) {
-				polymer.insert(i, r);
-			}
-			last = *i++;
-		}
-	}
-
-	std::map<char, int> counts;
-	for (auto c : polymer) {
-		counts[c]++;
-	}
+	auto counts = reaction.iterate(iterations);
 	auto nmin = counts.begin()->second;
 	auto nmax = nmin;
 	for (auto [elem, count] : counts) {
@@ -44,7 +78,9 @@ void solveFile(char const* fname) {
 
 void main() {
 	print("example");
-	solveFile("example.txt");
+	solveFile("example.txt", 10);
+	solveFile("example.txt", 40);
 	print("input");
-	solveFile("input.txt");
+	solveFile("input.txt", 10);
+	solveFile("input.txt", 40);
 }
